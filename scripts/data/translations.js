51fbe668a29dc69dcc5c25f87271af5cf9b7b487
cosylanguages <cosylanguages@gmail.com>
2025-05-25 21:06:00 +0200

    Update translations.js

--- scripts/data/translations.js
+++ scripts/data/translations.js
@@ -59,3 +59,96 @@ const grammarOptionsText = {
   ТАКОЙрусский: { to_have: "Иметь", possessives: "Притяжательные Прилагательные" },
   ԾՈՍՅհայկական: { to_have: "Ունենալ", possessives: "Ստացական Ածականներ" }
 };
+
+const questionVariants = {
+  COSYenglish: {
+    day2: [
+      ["Who is it?", "Who is this person?"],
+      ["What is it?", "What is this thing?"]
+    ],
+    day3plus: [
+      ["Who is it?", "Who is this person?"],
+      ["What is it?", "What does this person have?", "What is this thing?"]
+    ]
+  },
+  COSYfrançais: {
+    day2: [
+      ["C'est qui?", "Qui est cette personne?"],
+      ["C'est quoi?", "Qu'est-ce que c'est? (cette chose)" ]
+    ],
+    day3plus: [
+      ["C'est qui?", "Qui est cette personne?"],
+      ["C'est quoi?", "Qu'est-ce que cette personne a?", "Qu'est-ce que c'est? (cette chose)" ]
+    ]
+  },
+  COSYitaliano: {
+    day2: [
+      ["Chi è?", "Chi è questa persona?"],
+      ["Cos'è?", "Che cos'è questa cosa?" ]
+    ],
+    day3plus: [
+      ["Chi è?", "Chi è questa persona?"],
+      ["Cos'è?", "Che cosa ha questa persona?", "Che cos'è questa cosa?" ]
+    ]
+  },
+  COSYespañol: {
+    day2: [
+      ["¿Quién es?", "¿Quién es esta persona?"],
+      ["¿Qué es?", "¿Qué es esta cosa?" ]
+    ],
+    day3plus: [
+      ["¿Quién es?", "¿Quién es esta persona?"],
+      ["¿Qué es?", "¿Qué tiene esta persona?", "¿Qué es esta cosa?" ]
+    ]
+  },
+  COSYportuguês: {
+    day2: [
+      ["Quem é?", "Quem é esta pessoa?"],
+      ["O que é?", "O que é esta coisa?" ]
+    ],
+    day3plus: [
+      ["Quem é?", "Quem é esta pessoa?"],
+      ["O que é?", "O que esta pessoa tem?", "O que é esta coisa?" ]
+    ]
+  },
+  COSYdeutsch: {
+    day2: [
+      ["Wer ist das?", "Wer ist diese Person?"],
+      ["Was ist das?", "Was ist dieses Ding?" ]
+    ],
+    day3plus: [
+      ["Wer ist das?", "Wer ist diese Person?"],
+      ["Was ist das?", "Was hat diese Person?", "Was ist dieses Ding?" ]
+    ]
+  },
+  ΚΟΖΥελληνικά: {
+    day2: [
+      ["Ποιος είναι;", "Ποιος είναι αυτό το άτομο;"],
+      ["Τι είναι;", "Τι είναι αυτό το πράγμα;" ]
+    ],
+    day3plus: [
+      ["Ποιος είναι;", "Ποιος είναι αυτό το άτομο;"],
+      ["Τι είναι;", "Τι έχει αυτό το άτομο;", "Τι είναι αυτό το πράγμα;" ]
+    ]
+  },
+  ТАКОЙрусский: {
+    day2: [
+      ["Кто это?", "Кто этот человек?"],
+      ["Что это?", "Что это за вещь?" ]
+    ],
+    day3plus: [
+      ["Кто это?", "Кто этот человек?"],
+      ["Что это?", "Что есть у этого человека?", "Что это за вещь?" ]
+    ]
+  },
+  ԾՈՍՅհայկական: {
+    day2: [
+      ["Ո՞վ է սա:", "Ո՞վ է այս մարդը:"],
+      ["Ի՞նչ է սա:", "Ի՞նչ է այս բանը:" ]
+    ],
+    day3plus: [
+      ["Ո՞վ է սա:", "Ո՞վ է այս մարդը:"],
+      ["Ի՞նչ է սա:", "Ի՞նչ ունի այս մարդը:", "Ի՞նչ է այս բանը:" ]
+    ]
+  }
+};
