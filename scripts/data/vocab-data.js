3a94fdd7d427376cac3a561f3bace8788283b3de
cosylanguages <cosylanguages@gmail.com>
2025-05-25 09:56:01 +0200

    Update vocab-data.js

--- scripts/data/vocab-data.js
+++ scripts/data/vocab-data.js
@@ -21,29 +21,11 @@ const vocabData = {
     2: ["Il ragazzo", "La ragazza", "Il professore", "La professoressa", "Lo studente", "La studentessa", "La donna", "L'uomo"],
     3: ["la madre", "Il padre", "Il figlio", "La figlia", "La sorella", "Il fratello", "Il marito", "La moglie", "L'amico", "L'amica"]
   },
-  COSYespañol: {
-    1: ["Hola", "Adiós", "Sí", "No", "Gracias", "De nada"],
-    2: ["El chico", "La chica", "El profesor", "La profesora", "El estudiante", "La estudiante", "La mujer", "El hombre"]
-  },
-  COSYportuguês: {
-    1: [],
-    2: []
-  },
-  COSYdeutsch: {
-    1: ["Hallo", "Auf Wiedersehen", "Ja", "Nein", "Danke", "Bitte"],
-    2: ["Der Junge", "Das Mädchen", "Der Lehrer", "Die Lehrerin", "Der Student", "Die Studentin", "Die Frau", "Der Mann"]
-  },
   ΚΟΖΥελληνικά: {
     1: ["Γειά σου", "Αντίο", "Όχι", "Ναι", "Ευχαριστώ", "Παρακαλώ"],
     2: ["Το αγόρι", "Το κορίτσι", "Ο δάσκαλος", "Η δασκάλα", "Ο μαθητής", "Η μαθήτρια", "Η γυναίκα", "Ο άντρας"],
     3: ["Η μητέρα", "Ο πατέρας", "Ο γιος", "Η κόρη", "Η αδερφή", "Ο αδερφός", "Η σύζυγος", "Ο σύζυγος", "Ο φίλος", "η Φίλη"]
   },
   ТАКОЙрусский: {
-    1: ["Привет", "До свидания", "Да", "Нет", "Спасибо", "Пожалуйста"],
-    2: ["Мальчик", "Девочка", "Учитель", "Учительница", "Студент", "Студентка", "Женщина", "Мужчина"]
-  },
-  ԾՈՍՅհայկական: {
-    1: [],
-    2: []
   }
 };
