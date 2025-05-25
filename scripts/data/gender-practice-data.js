814956bef056dc37b76f46e3cc979a52c8ab64b2
cosylanguages <cosylanguages@gmail.com>
2025-05-25 16:31:10 +0200

    Update gender-practice-data.js

--- scripts/data/gender-practice-data.js
+++ scripts/data/gender-practice-data.js
@@ -113,11 +113,188 @@ const genderPracticeData = {
       { word: "οικογένεια", article: "η" }
     ]
   },
-  COSYportuguês: {
+  ТАКОЙрусский: {
     1: [
+      { word: "вода", article: "одна" },
+      { word: "сок", article: "один" },
+      { word: "пиво", article: "одно" },
+      { word: "вино", article: "одно" },
+      { word: "кофе", article: "одно" },
+      { word: "чай", article: "один" },
+      { word: "карта", article: "одна" }
     ],
     2: [
+      { word: "учитель", article: "один" },
+      { word: "студент", article: "один" },
+      { word: "студентка", article: "одна" },
+      { word: "мальчик", article: "один" },
+      { word: "женщина", article: "одна" },
+      { word: "мужчина", article: "один" },
+      { word: "девочка", article: "одна" },
+      { word: "вещь", article: "одна" },
+      { word: "человек", article: "один" },
+      { word: "напиток", article: "один" },
+      { word: "проблема", article: "одна" }
+    ],
+    3: [
+      { word: "мать", article: "одна" },
+      { word: "отец", article: "один" },
+      { word: "брат", article: "один" },
+      { word: "сестра", article: "одна" },
+      { word: "сын", article: "один" },
+      { word: "дочь", article: "одна" },
+      { word: "муж", article: "один" },
+      { word: "жена", article: "одна" },
+      { word: "друг", article: "один" },
+      { word: "подруга", article: "одна" },
+      { word: "семья", article: "одна" }
+    ]
+  },
+  COSYdeutsch: {
+    1: [
+      { word: "Wasser", article: "das" },
+      { word: "Saft", article: "der" },
+      { word: "Bier", article: "das" },
+      { word: "Wein", article: "der" },
+      { word: "Kaffee", article: "der" },
+      { word: "Tee", article: "der" },
+      { word: "Karte", article: "die" }
+    ],
+    2: [
+      { word: "Lehrer", article: "der" },
+      { word: "Schüler", article: "der" },
+      { word: "Schülerin", article: "die" },
+      { word: "Junge", article: "der" },
+      { word: "Frau", article: "die" },
+      { word: "Mann", article: "der" },
+      { word: "Mädchen", article: "das" },
+      { word: "Ding", article: "das" },
+      { word: "Person", article: "die" },
+      { word: "Getränk", article: "das" },
+      { word: "Problem", article: "das" }
+    ],
+    3: [
+      { word: "Mutter", article: "die" },
+      { word: "Vater", article: "der" },
+      { word: "Bruder", article: "der" },
+      { word: "Schwester", article: "die" },
+      { word: "Sohn", article: "der" },
+      { word: "Tochter", article: "die" },
+      { word: "Ehemann", article: "der" },
+      { word: "Ehefrau", article: "die" },
+      { word: "Freund", article: "der" },
+      { word: "Freundin", article: "die" },
+      { word: "Familie", article: "die" }
+    ]
+  },
+  COSYespañol: {
+    1: [
+      { word: "agua", article: "el" },
+      { word: "jugo", article: "el" },
+      { word: "cerveza", article: "la" },
+      { word: "vino", article: "el" },
+      { word: "café", article: "el" },
+      { word: "té", article: "el" },
+      { word: "tarjeta", article: "la" }
+    ],
+    2: [
+      { word: "profesor", article: "el" },
+      { word: "estudiante", article: "el/la" },
+      { word: "niño", article: "el" },
+      { word: "hombre", article: "el" },
+      { word: "mujer", article: "la" },
+      { word: "chica", article: "la" },
+      { word: "cosa", article: "la" },
+      { word: "persona", article: "la" },
+      { word: "bebida", article: "la" },
+      { word: "problema", article: "el" }
+    ],
+    3: [
+      { word: "madre", article: "la" },
+      { word: "padre", article: "el" },
+      { word: "hermano", article: "el" },
+      { word: "hermana", article: "la" },
+      { word: "hijo", article: "el" },
+      { word: "hija", article: "la" },
+      { word: "esposo", article: "el" },
+      { word: "esposa", article: "la" },
+      { word: "amigo", article: "el" },
+      { word: "amiga", article: "la" },
+      { word: "familia", article: "la" }
+    ]
+  },
+  COSYportuguês: {
+    1: [
+      { word: "água", article: "a" },
+      { word: "suco", article: "o" },
+      { word: "cerveja", article: "a" },
+      { word: "vinho", article: "o" },
+      { word: "café", article: "o" },
+      { word: "chá", article: "o" },
+      { word: "cartão", article: "o" }
+    ],
+    2: [
+      { word: "professor", article: "o" },
+      { word: "estudante", article: "o/a" },
+      { word: "menino", article: "o" },
+      { word: "homem", article: "o" },
+      { word: "mulher", article: "a" },
+      { word: "garota", article: "a" },
+      { word: "coisa", article: "a" },
+      { word: "pessoa", article: "a" },
+      { word: "bebida", article: "a" },
+      { word: "problema", article: "o" }
+    ],
+    3: [
+      { word: "mãe", article: "a" },
+      { word: "pai", article: "o" },
+      { word: "irmão", article: "o" },
+      { word: "irmã", article: "a" },
+      { word: "filho", article: "o" },
+      { word: "filha", article: "a" },
+      { word: "marido", article: "o" },
+      { word: "esposa", article: "a" },
+      { word: "amigo", article: "o" },
+      { word: "amiga", article: "a" },
+      { word: "família", article: "a" }
+    ]
+  },
+  'ԾՈՍՅհայկական': {
+    1: [
+      { word: "ջուր", article: "ը" },
+      { word: "սուրճ", article: "ը" },
+      { word: "գարեջուր", article: "ը" },
+      { word: "գինի", article: "ը" },
+      { word: "սուրճ", article: "ը" },
+      { word: "թեյ", article: "ը" },
+      { word: "քարտ", article: "ը" }
+    ],
+    2: [
+      { word: "դասախոս", article: "ը" },
+      { word: "ուսանող", article: "ը" },
+      { word: "տղամարդ", article: "ը" },
+      { word: "աղջիկ", article: "ը" },
+      { word: "կին", article: "ը" },
+      { word: "տղա", article: "ը" },
+      { word: "դասախոսուհի", article: "ը" },
+      { word: "ուսանողուհի", article: "ը" },
+      { word: "առարկա", article: "ը" },
+      { word: "անձ", article: "ը" },
+      { word: "խմիչք", article: "ը" },
+      { word: "խնդիր", article: "ը" }
+    ],
+    3: [
+      { word: "մայր", article: "ը" },
+      { word: "հայր", article: "ը" },
+      { word: "եղբայր", article: "ը" },
+      { word: "քույր", article: "ը" },
+      { word: "որդի", article: "ը" },
+      { word: "դուստր", article: "ը" },
+      { word: "ամուսին", article: "ը" },
+      { word: "կինը", article: "ը" },
+      { word: "ընկեր", article: "ը" },
+      { word: "ընկերուհի", article: "ը" },
+      { word: "ընտանիք", article: "ը" }
     ]
   }
-  // Add data for other languages
 };
