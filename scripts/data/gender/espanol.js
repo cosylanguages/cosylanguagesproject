b66b7b6510a8e399f1fdbf388f77b0b831bd8fc5
cosylanguages <cosylanguages@gmail.com>
2025-05-26 16:21:17 +0200

    yay

--- scripts/data/gender/espanol.js
+++ scripts/data/gender/espanol.js
@@ -1,3 +1,25 @@
 export const genderPracticeDataEspanol = {
+  1: [
+    { word: "agua", article: "el" },
+    { word: "jugo", article: "el" },
+    { word: "cerveza", article: "la" },
+    { word: "vino", article: "el" },
+    { word: "café", article: "el" },
+    { word: "té", article: "el" },
+    { word: "tarjeta", article: "la" } 
+    ],
+   2: [
+    { word: "profesor", article: "el" },
+    { word: "estudiante", article: "el" },
+    { word: "hombre", article: "el" },
+    { word: "niño", article: "el" },
+    { word: "mujer", article: "la" },
+    { word: "estudiante (femenino)", article: "la" },
+    { word: "profesora", article: "la" },
+    { word: "niña", article: "la" },
+    { word: "cosa", article: "la" },
+    { word: "persona", article: "la" },
+    { word: "bebida", article: "la" }
+    ]
   // Add Spanish gender practice data here as needed
 };
--- scripts/data/gender/portugues.js
+++ scripts/data/gender/portugues.js
@@ -1,3 +1,25 @@
 export const genderPracticeDataPortugues = {
+  1: [
+    { word: "água", article: "a" },
+    { word: "suco", article: "o" },
+    { word: "cerveja", article: "a" },
+    { word: "vinho", article: "o" },
+    { word: "café", article: "o" },
+    { word: "chá", article: "o" },
+    { word: "cartão", article: "o" }
+    ],
+2: [    
+    { word: "professor", article: "o" },
+    { word: "aluno", article: "o" },
+    { word: "homem", article: "o" },
+    { word: "menino", article: "o" },
+    { word: "mulher", article: "a" },
+    { word: "aluna", article: "a" },
+    { word: "professora", article: "a" },
+    { word: "menina", article: "a" },
+    { word: "coisa", article: "a" },
+    { word: "pessoa", article: "a" },
+    { word: "bebida", article: "a" }
+    ]
   // Add Portuguese gender practice data here as needed
 };
